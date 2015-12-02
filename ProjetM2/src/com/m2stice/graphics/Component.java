14aafea7c7636fc08f3e4afc842e48cc57f170f4
litlleL@litlleL-Pc <litlleL@litlleL-Pc>
2015-12-02 11:26:46 -0400

    src

---
+++ ProjetM2/src/com/m2stice/graphics/Component.java
@@ -0,0 +1,14 @@
+package com.m2stice.graphics;
+
+public class Component {
+
+	public Component() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ok");
+	}
+
+}
