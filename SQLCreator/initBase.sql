ab0fa6e67789c2381459537bc8baf1fff024ba5d
Emmanuel <Emmanuel@m2stice>
2015-12-03 11:12:40 -0400

    Détails

--- SQLCreator/initBase.sql
+++ SQLCreator/initBase.sql
@@ -1,4 +1,5 @@
---Création de la base M2STICE:
+/*Création de la base M2STICE:*/
+/*Attention le nom de la base de données en ligne est u960093295_stice*/
 create database M2STICE character 'utf8';
 
 /*Création de la table diplome*/
