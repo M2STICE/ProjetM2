05a8650f5640470d9846dffffbd913c9e404c3b1
Didier <Didier@m2stice>
2015-12-03 10:25:49 -0600

    Correction des ordres sql create et mise dans l'ordre de création des tables.

--- SQLCreator/initBase.sql
+++ SQLCreator/initBase.sql
@@ -26,49 +26,6 @@ mot_de_passe varchar(100)
 )
 ENGINE=INNODB;
 
---Création de la table compétences
-create table competence(
-	code_competence int primary key, 
-	nom_competence varchar(255),
-	code_domaine int, 
-	code_EC int,
-	foreign key(code_domaine) references competence(code_domaine),
-	foreign key(code_EC) references ec(code_EC)
-)
-ENGINE=INNODB;
-
---Création de la table item
-create table item(
-	code_item int primary key, 
-	nom_item varchar(255),
-	code_competence int, 
-	code_evaluation int,
-	foreign key(code_competence) references competence(code_competence),
-	foreign key(code_evaluation) references evaluation(code_evaluation)
-)
-ENGINE=INNODB;
-
---Création de la table sous-item
-create table sous_item(
-	code_sous_item int primary key, 
-	nom_sous_item varchar(255), 
-	code_item int,
-	foreign key(code_item) references item(code_item)
-)
-ENGINE=INNODB;
-
---Création de la table evaluation
-create table evaluation(
-	code_evaluation int primary key, 
-	nom_evaluation varchar(255), 
-	note_maximale float, 
-	coefficient_evaluation float, 
-	type_epreuve varchar(255),
-	code_sous_item int,
-	foreign key(code_sous_item) references sous_item(code_sous_item)
-)
-ENGINE=INNODB;
-
 --Création de la table année
 create table annee(
 code_annee int primary key, 
@@ -76,10 +33,14 @@ nom_annee varchar(255)
 )
 ENGINE=INNODB;
 
+
+
+
 --Création de la table 
 create table diplome_annee(
 	code_diplome int primary key, 
-	code_annee int foreign key 
+	code_annee int,
+	foreign key(code_annee) references annee(code_annee)
 )
 ENGINE=INNODB;
 
@@ -92,8 +53,8 @@ foreign key(code_annee) references annee(code_annee)
 )
 ENGINE=INNODB;
 
---Création table UC
-create table uc(
+--Création table UE
+create table ue(
 code_ue int primary key,
 nom_ue varchar(100),
 nombre_ects int,
@@ -120,7 +81,18 @@ resume_ec varchar(1000),
 code_ue int,
 responsable_ec int,
 foreign key(code_ue) references ue(code_ue),
-foreign key(responsable_ec) references ec(responsable_ec)
+foreign key(responsable_ec) references intervenant(code_intervenant)
+)
+ENGINE=INNODB;
+
+--Création de la table compétences
+create table competence(
+	code_competence int primary key, 
+	nom_competence varchar(255),
+	code_domaine int, 
+	code_ec int,
+	foreign key(code_domaine) references domaine(code_domaine),
+	foreign key(code_ec) references ec(code_ec)
 )
 ENGINE=INNODB;
 
@@ -130,6 +102,57 @@ code_etudiant int primary key,
 nom_etudiant varchar(100),
 prenom_etudiant varchar(100),
 mot_de_passe_etudiant varchar(100)
+)
+ENGINE=INNODB;
+
+--Création table intervenant_ec
+create table intervenant_ec(
+code_intervenant int,
+code_ec int,
+primary key(code_intervenant,code_ec),
+foreign key(code_intervenant) references intervenant(code_intervenant),
+foreign key(code_ec) references ec(code_ec)
+)
+ENGINE=INNODB;
+
+--Création table etudiant_diplome
+create table etudiant_diplome(
+code_etudiant int, 
+code_diplome int,
+primary key (code_etudiant,code_diplome),
+foreign key(code_etudiant)references etudiant(code_etudiant),
+foreign key(code_diplome)references diplome(code_diplome)
+)
+ENGINE=INNODB;
+
+--Création de la table item
+create table item(
+	code_item int primary key, 
+	nom_item varchar(255),
+	code_competence int, 
+	code_evaluation int,
+	foreign key(code_competence) references competence(code_competence) 
+)ENGINE=INNODB;
+
+--Création de la table sous-item
+create table sous_item(
+	code_sous_item int primary key, 
+	nom_sous_item varchar(255), 
+	code_item int,
+	foreign key(code_item) references item(code_item)
+)
+ENGINE=INNODB;
+
+
+--Création de la table evaluation
+create table evaluation(
+	code_evaluation int primary key, 
+	nom_evaluation varchar(255), 
+	note_maximale float, 
+	coefficient_evaluation float, 
+	type_epreuve varchar(255),
+	code_sous_item int,
+	foreign key(code_sous_item) references sous_item(code_sous_item)
 )
 ENGINE=INNODB;
 
@@ -145,20 +168,5 @@ foreign key(code_evaluation) references evaluation(code_evaluation),
 ENGINE=INNODB;
 
 
---Création table intervenant_ec
-create table intervenant_ec(
-code_intervenant int primary key,
-code_ec int primary key,
-foreign key(code_intervenant) references intervenant(code_intervenant),
-foreign key(code_ec) references ec(code_ec)
-)
-ENGINE=INNODB;
 
---Création table etudiant_diplome
-create table etudiant_diplome(
-code_etudiant int primary key, 
-code_diplome int primary key, 
-foreign key(code_etudiant)references etudiant(code_etudiant),
-foreign key(code_diplome)references diplome(code_diplome)
-)
-ENGINE=INNODB;
+
