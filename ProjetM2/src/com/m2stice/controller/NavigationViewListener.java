81dbe3192b7efefab26f005744a69dda575ff227
fabiancisnerosbridon <fabiancisnerosbridon@air-de-fabian.connectify>
2015-12-14 14:26:30 -0400

    Domaine

--- ProjetM2/src/com/m2stice/controller/NavigationViewListener.java
+++ ProjetM2/src/com/m2stice/controller/NavigationViewListener.java
@@ -1,11 +1,15 @@
 package com.m2stice.controller;
 
+import java.util.LinkedList;
+
+import com.m2stice.adapter.DomaineAdapter;
 import com.m2stice.graphics.CompetenceView;
 import com.m2stice.graphics.Interface;
 import com.m2stice.graphics.NavigationView;
 import com.m2stice.graphics.ResultatView;
 import com.m2stice.graphics.SyllabusView;
 import com.m2stice.model.Diplome;
+import com.m2stice.model.Domaine;
 
 public class NavigationViewListener {
 	
@@ -28,11 +32,16 @@ public class NavigationViewListener {
 		this.syllabusView = syllabusView;
 	}
 	
-	public void setDomaine(){
-		navigationView.diplome_courant.getCode();
-		//SQL
+	public void setDomaine()
+	{
+		int codeDiplome = navigationView.diplome_courant.getCode();
 		
-		//competenceView.setDomaineJTable(ld);
+		String requete = "select * from domaine "
+				+ "inner join diplome on "
+				+ "domaine.code_diplome = diplome.code_diplome "
+				+ "where code_diplome = " + codeDiplome + ";";
+		
+		competenceView.setDomaineJTable(interfaceUtilisateur.getController().getDomaine(requete));
 	}
 	
 
