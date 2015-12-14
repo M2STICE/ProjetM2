421a47edc587e4f5ff2304695c6e3ac80bb903ce
Emmanuel <Emmanuel@TheEm>
2015-12-14 12:12:47 -0400

    Modification sur NavigationViewListener

--- ProjetM2/src/com/m2stice/controller/NavigationViewListener.java
+++ ProjetM2/src/com/m2stice/controller/NavigationViewListener.java
@@ -1,5 +1,23 @@
 package com.m2stice.controller;
 
+import com.m2stice.graphics.CompetenceView;
+import com.m2stice.graphics.Interface;
+import com.m2stice.graphics.NavigationView;
+import com.m2stice.graphics.ResultatView;
+import com.m2stice.graphics.SyllabusView;
+
 public class NavigationViewListener {
+	
+	private Interface interfaceUtilisateur;
+	private ResultatView resultatView;
+	private NavigationView navigationView;
+	private SyllabusView syllabusView;
+	private CompetenceView competenceView;
+	
+	public NavigationViewListener(Interface interfaceUtilisateur,NavigationView navigationView){
+		this.interfaceUtilisateur = interfaceUtilisateur;
+		this.navigationView = navigationView;
+	}
+	
 
 }
